a4b8e47eaf3ab4e5e28c43db6a6789223bfff34a
James Harding <james@jamesharding.me>
2016-05-15 20:22:09 +0100

    Generate wallboard & add async for each

--- app.js
+++ app.js
@@ -2,9 +2,23 @@ var googleapis = require('googleapis'),
     JWT = googleapis.auth.JWT,
     analytics = googleapis.analytics('v3'),
     config = require('./config/config'),
-    queries = require('./config/queries.json');
+    data = require('./config/data.json'),
+    queries = data.queries,
+    forEach = require('async-foreach').forEach,
+    request = require('request');
 
-
+var wb = {
+    title: data.title,
+    url_slug: data.url,
+    autoLayout: true,
+    elems: [
+        {
+            id: 'wb_0',
+            tagName: 'H1',
+            innerText: data.title
+        }
+    ]
+};
 
 var authClient = new JWT(
     config.GA.SERVICE_ACCOUNT_EMAIL,
@@ -19,14 +33,35 @@ authClient.authorize(function(err, tokens) {
         return;
     }
 
-    queries.forEach(function(q) {
-        q.auth = authClient;
-        analytics.data.ga.get(q, function(err, res) {
+    forEach(queries, function(item, index, arr) {
+        item.params.auth = authClient;
+        
+        
+        analytics.data.ga.get(item.params, function(err, res) {
             if (err) console.log(err);
-            console.log(res.rows);
+            else {
+                var rows = [item.thead];
+                Array.prototype.push.apply(rows, res.rows);
+
+                wb.elems.push({
+                    id: 'wb_'+ index,
+                    tagName: 'table',
+                    title: item.title,
+                    struct: { rows: rows }
+                });
+            }
+        });
+        var done = this.async();
+        setTimeout(function() {
+            done();
+        }, 500);
+
+    }, function(notAborted, res) {
+        console.log(wb);
+
+        request.post({url: config.WB_APP.URL + '/api/v1/upsert', form: {internal: false, wb: wb}}, function(err){
+            if (!err) return true;
         });
     });
 
 });
-
-
